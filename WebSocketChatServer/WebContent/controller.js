d63f52c50c7ca93f3af44cc63b4eb60240e70089
linhha98 <57082223+linhha98@users.noreply.github.com>
2019-11-04 11:03:56 +0700

    update

--- WebSocketChatServer/WebContent/controller.js
+++ WebSocketChatServer/WebContent/controller.js
@@ -109,3 +109,71 @@ function sendMessage() {
 		}
 	}
 }
+function createGroupChat() {
+	//listFriendID.push(5); //Giả sử ID người dùng hiện tại là 5
+	var groupName = $('#textGroupChatName').val();
+	var dataRequest = {
+		'groupName' : groupName,
+		'listID' : listFriendID,
+		'function' : 'CREATE_GROUP'
+	};
+	console.log(dataRequest);
+	websocket.send(JSON.stringify(dataRequest));
+}
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+
+
+// Get the modal
+var createGroup = document.getElementById("createGroup");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+	createGroup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	createGroup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == createGroup) {
+		createGroup.style.display = "none";
+	}
+}
+
